7d8bf5dab6de0951f820e82e36eb2fd381bfa358
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-03-23 22:15:27 +0800

    Create MultiHeadAttention.py

---
+++ MultiHeadAttention.py
@@ -0,0 +1,45 @@
+# @title MultiHeadAttention
+import torch
+import torch.nn as nn
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads=None, d_head=8, cond_dim=None, dropout=0.): # .1
+        super().__init__()
+        self.d_model = d_model
+        self.d_head = d_head
+        self.n_heads = d_model // d_head
+        # self.d_head = d_model // n_heads
+        self.cond_dim = cond_dim
+        self.q = nn.Linear(d_model, d_model, bias=False)
+        self.kv = nn.Linear(cond_dim or d_model, 2*d_model, bias=False)
+        # self.k = nn.Sequential(nn.Dropout(dropout), nn.Linear(cond_dim, d_model, bias=False))
+        self.lin = nn.Linear(d_model, d_model)
+        # self.lin = zero_module(nn.Linear(d_model, d_model))
+        # self.lin = nn.Sequential(nn.Dropout(dropout), zero_module(nn.Linear(d_model, d_model)))
+        self.drop = nn.Dropout(dropout) # indp before q,k,v; after linout
+        self.scale = self.d_head ** -.5
+
+    def forward(self, x, cond=None, mask=None): # [batch, T, d_model]=[batch, h*w, c], [batch, num_tok, cond_dim], [batch,T]
+        batch = x.shape[0]
+        if self.cond_dim==None: cond=x # is self attn
+        Q = self.q(x).view(batch, -1, self.n_heads, self.d_head).transpose(1, 2) # [batch, T, d_model] -> [batch, n_heads, T, d_head]
+        # K = self.k(x).view(batch, -1, self.n_heads, self.d_head).transpose(1, 2)
+        K, V = self.kv(cond).view(batch, -1, self.n_heads, 2*self.d_head).transpose(1, 2).chunk(2, dim=-1) # [batch, n_heads, T/num_tok, d_head]
+
+        # # linear attention # Softmax(Q) @ (Softmax(K).T @ V)
+        if mask != None:
+            mask = mask[:, None, :, None] # [batch,T] -> [batch,1,T,1]
+            K, V = K.masked_fill(mask, -torch.finfo(x.dtype).max), V.masked_fill(mask, -torch.finfo(x.dtype).max)
+        Q, K = Q.softmax(dim=-1)*self.scale, K.softmax(dim=-2)
+        context = K.transpose(-2,-1) @ V # [batch, n_heads, d_head, d_head]
+        out = Q @ context # [batch, n_heads, T/num_tok, d_head]
+
+        # # (quadratic) attention # Softmax(Q @ K.T) @ V
+        # attn = Q @ K.transpose(-2,-1) * self.scale # [batch, n_heads, T] # [batch, n_heads, T, T/num_tok]
+        # if mask != None: attn = attn.masked_fill(mask[:, None, :, None], -torch.finfo(attn.dtype).max) # [batch,T]->[batch,1,T,1]
+        # attention = torch.softmax(attn, dim=-1)
+        # out = self.drop(attention) @ V # [batch, n_heads, T, d_head]
+
+        out = out.transpose(1, 2).flatten(2)
+        return self.drop(self.lin(out)) # [batch, T, d_model]
