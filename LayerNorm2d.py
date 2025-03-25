a98df6ee109feb1638549d838783581d296a8300
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-03-25 19:49:02 +0800

    Update LayerNorm2d.py

--- LayerNorm2d.py
+++ LayerNorm2d.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 
+class LayerNorm2d(nn.RMSNorm): # LayerNorm RMSNorm
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def forward(self, x): return super().forward(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+
 # class LayerNorm2d(nn.LayerNorm):
 class LayerNorm2d(nn.RMSNorm):
     def __init__(self, *args, **kwargs):
