47891c976e52f0d5e65b2ee552d141df28168b27
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-03-26 15:24:16 +0800

    Update UIB_me.py

--- UIB_me.py
+++ UIB_me.py
@@ -12,20 +12,22 @@ class UIB(nn.Module):
         super().__init__()
         act = nn.SiLU()
         out_ch = out_ch or in_ch
+        self.res_conv = nn.Conv2d(in_ch, out_ch, 1) if in_ch != out_ch else nn.Identity()
         self.conv = nn.Sequential( # norm,act,conv
             nn.BatchNorm2d(in_ch), act, nn.Conv2d(in_ch, in_ch, kernel, 1, kernel//2, groups=in_ch, bias=False),
             nn.BatchNorm2d(in_ch), act, nn.Conv2d(in_ch, mult*in_ch, 1, bias=False),
             nn.BatchNorm2d(mult*in_ch), act, nn.Conv2d(mult*in_ch, mult*in_ch, kernel, 1, kernel//2, groups=mult*in_ch, bias=False),
             nn.BatchNorm2d(mult*in_ch), act, zero_module(nn.Conv2d(mult*in_ch, out_ch, 1, bias=False)),
+            # nn.BatchNorm2d(mult*in_ch), act, nn.Conv2d(mult*in_ch, out_ch, 1, bias=False),
         )
 
     def forward(self,x):
-        return self.conv(x)
+        return self.conv(x) + self.res_conv(x)
 
-# # in_ch, out_ch = 16,3
-# in_ch, out_ch = 3,16
-# model = UIB(in_ch, out_ch)
-# x = torch.rand(128, in_ch, 64, 64)
-# out = model(x)
-# print(out.shape)
-# # print(out)
+# in_ch, out_ch = 16,3
+in_ch, out_ch = 3,16
+model = UIB(in_ch, out_ch)
+x = torch.rand(128, in_ch, 64, 64)
+out = model(x)
+print(out.shape)
+# print(out)
