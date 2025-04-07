8a2aa500de1d48db2a43729679c04ddcccd14716
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-04-07 23:07:24 +0800

    Create ResBlock.py

---
+++ ResBlock.py
@@ -0,0 +1,21 @@
+# @title ResBlock
+import torch
+import torch.nn as nn
+
+class ResBlock(nn.Module):
+    def __init__(self, in_ch, out_ch=None, kernel=3):
+        super().__init__()
+        out_ch = out_ch or in_ch
+        act = nn.SiLU() #
+        self.res_conv = nn.Conv2d(in_ch, out_ch, 1) if in_ch != out_ch else nn.Identity()
+        # self.block = nn.Sequential( # best?
+        #     nn.Conv2d(in_ch, out_ch, 3, padding=1), nn.BatchNorm2d(out_ch), act,
+        #     zero_module(nn.Conv2d(out_ch, out_ch, 3, padding=1)), nn.BatchNorm2d(out_ch), act,
+        #     )
+        self.block = nn.Sequential(
+            nn.BatchNorm2d(in_ch), act, nn.Conv2d(in_ch, out_ch, kernel, padding=kernel//2),
+            nn.BatchNorm2d(out_ch), act, zero_module(nn.Conv2d(out_ch, out_ch, kernel, padding=kernel//2)),
+            )
+
+    def forward(self, x): # [b,c,h,w]
+        return self.block(x) + self.res_conv(x)
