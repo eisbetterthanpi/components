08043eceee5a4895c0db7f53ad481e941bc9b89e
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-04-08 09:22:27 +0800

    Create AdaptivePool_at.py

---
+++ AdaptivePool_at.py
@@ -0,0 +1,16 @@
+# @title hiera vit me
+import torch
+import torch.nn as nn
+
+class AdaptivePool_at(nn.AdaptiveAvgPool1d): # AdaptiveAvgPool1d AdaptiveMaxPool1d
+    def __init__(self, dim=1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dim=dim
+    def forward(self, x):
+        x = x.transpose(self.dim,-1)
+        shape = x.shape
+        return super().forward(x.flatten(0,-2)).unflatten(0, shape[:-1]).transpose(self.dim,-1)
+
+# x = torch.rand(2,3,4,4)
+# ch_pool = AdaptivePool_at(1, output_size=5)
+# print(ch_pool(x).shape)
