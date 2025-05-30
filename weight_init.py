73069e6d8ffeb638daf80ee34c50cb885e4ffda8
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-05-30 15:20:11 +0800

    Create weight_init.py

---
+++ weight_init.py
@@ -0,0 +1,35 @@
+
+import math
+import torch
+import torch.nn as nn
+
+def StableInit(m): # https://openreview.net/pdf?id=lkRjnNW0gb
+    if isinstance(m, nn.Linear):
+        # W ~ N(0, ( 1/ (sqrt(n_in) + sqrt(n_out)) )^2 )
+        # want std = 1/ (sqrt(n_in) + sqrt(n_out))
+        # n_in, n_out = module.weight.shape[0], module.weight.shape[1]
+        n_in, n_out = m.weight.shape
+        nn.init.normal_(m.weight, std=1/(math.sqrt(n_in)+math.sqrt(n_out)))
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+
+
+        self.embed.apply(self.init_conv)
+        self.embed.apply(self.init_weights)
+
+    def init_conv(self, m):
+        if isinstance(m, nn.Conv1d):
+            # nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5))
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if m.bias is not None:
+                # bound = 1 / math.sqrt(m.in_channels * m.kernel_size * m.kernel_size)
+                # nn.init.uniform_(m.bias, -bound, bound)
+                nn.init.zeros_(m.bias)
+
+    def init_weights(self, m):
+        if isinstance(m, (nn.Linear, nn.Conv1d)):
+            torch.nn.init.normal_(m.weight, std=.02)
+            if m.bias is not None: nn.init.zeros_(m.bias)
+
+
+
