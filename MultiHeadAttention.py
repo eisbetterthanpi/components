b3dc6b8a074c5f192f0c1dcfc29c5d1388ef20b9
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-06-12 08:09:42 +0800

    Update MultiHeadAttention.py

--- MultiHeadAttention.py
+++ MultiHeadAttention.py
@@ -1,4 +1,4 @@
-# @title MultiHeadAttention
+# @title Attention
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,29 +10,28 @@ def zero_module(module):
     return module
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, n_heads=None, d_head=8, cond_dim=None, dropout=0.): # .1
+    # def __init__(self, d_model, n_heads=None, d_head=8, cond_dim=None, dropout=0.): # .1
+    def __init__(self, query_dim, cond_dim=None, n_heads=8, d_head=64, drop=0):
         super().__init__()
-        self.d_model, self.n_heads, self.d_head = d_model, n_heads, d_model // n_heads
+        # self.d_model, self.n_heads, self.d_head = d_model, n_heads, d_model // n_heads
+        d_model = d_head * n_heads
+        self.d_head, self.n_heads = d_head, n_heads
         self.cond_dim = cond_dim
-        self.q = nn.Linear(d_model, d_model, bias=False)
+        self.q = nn.Linear(query_dim, d_model, bias=False)
         self.kv = nn.Linear(cond_dim or d_model, 2*d_model, bias=False)
         self.lin = zero_module(nn.Linear(d_model, d_model))
         self.drop = nn.Dropout(dropout) # indp before q,k,v; after linout
         self.scale = self.d_head**-.5
+        # torch.nn.init.normal_(self.q.weight, std=.02)
+        # torch.nn.init.normal_(self.kv.weight, std=.02)
+        torch.nn.init.normal_(self.q.weight, std=1/(math.sqrt(query_dim)+math.sqrt(d_model)))
+        torch.nn.init.normal_(self.kv.weight, std=1/(math.sqrt(cond_dim or query_dim)+math.sqrt(d_model)))
 
     def forward(self, x, cond=None, mask=None): # [batch, T, d_model]=[batch, h*w, c], [batch, num_tok, cond_dim], [batch,T]
         if self.cond_dim==None: cond=x # is self attn
         q = self.q(x).unflatten(-1, (self.n_heads, self.d_head)).transpose(1, 2) # [batch, T, d_model] -> [batch, n_heads, T, d_head]
         # K = self.k(x).unflatten(-1, (self.n_heads, self.d_head)).transpose(1, 2)
         k, v = self.kv(cond).unflatten(-1, (self.n_heads, 2*self.d_head)).transpose(1, 2).chunk(2, dim=-1) # [batch, n_heads, T/num_tok, d_head]
-
-        # # linear attention # Softmax(q) @ (Softmax(k).T @ v)
-        # if mask != None:
-        #     mask = mask[:, None, :, None] # [batch,T] -> [batch,1,T,1]
-        #     k, v = k.masked_fill(mask, -torch.finfo(x.dtype).max), v.masked_fill(mask, -torch.finfo(x.dtype).max)
-        # q, k = q.softmax(dim=-1)*self.scale, k.softmax(dim=-2)
-        # context = k.transpose(-2,-1) @ v # [batch, n_heads, d_head, d_head]
-        # out = q @ context # [batch, n_heads, T/num_tok, d_head]
 
         # # (quadratic) attention # Softmax(q @ k.T) @ v
         out = F.scaled_dot_product_attention(q, k, v, attn_mask=mask.unsqueeze(1) if mask != None else None, dropout_p=0) # mask: [batch,len_q, len_v]
@@ -46,19 +45,71 @@ class MultiHeadAttention(nn.Module):
         out = out.transpose(1,2).flatten(2)
         return self.drop(self.lin(out)) # [batch, T, d_model]
 
-class EncoderLayer(nn.Module):
-    def __init__(self, d_model, n_heads, ff_dim, dropout=0):
+
+class SwiGLU(nn.Module): # https://arxiv.org/pdf/2002.05202
+    def __init__(self, d_model, ff_dim): # d_model * 3*ff_dim params
         super().__init__()
-        self.drop = nn.Dropout(dropout)
-        self.norm = nn.RMSNorm(d_model)
-        self.attn = MultiHeadAttention(d_model, n_heads, dropout=0)
+        self.lin0 = nn.Linear(d_model, 2*ff_dim, bias=False)
+        self.lin1 = zero_module(nn.Linear(ff_dim, d_model, bias=False))
+        # torch.nn.init.normal_(self.lin0.weight, std=.02)
+        torch.nn.init.normal_(self.lin0.weight, std=1/(math.sqrt(d_model)+math.sqrt(ff_dim)))
+
+    def forward(self, x): # [b,t,d]
+        x0, x1 = self.lin0(x).chunk(2, dim=-1)
+        return self.lin1(x0*F.silu(x1))
+
+
+class AttentionBlock(nn.Module):
+    # def __init__(self, d_model, cond_dim=None, d_head, ff_dim=None, dropout=0.):
+    def __init__(self, d_model, n_heads ,cond_dim=None, ff_dim=None, drop=0):
+        super().__init__()
+        self.d_model = d_model
+        self.cond_dim = cond_dim
+        self.norm1 = nn.RMSNorm(d_model) # LayerNorm RMSNorm
+        if cond_dim!=None: self.norm2 = nn.RMSNorm(cond_dim)
+        self.drop = nn.Dropout(drop)
+        self.attn = MultiHeadAttention(d_model, n_heads, dropout=dropout)
+        self.attn = Attention(d_model, cond_dim, n_heads=n_heads, d_head=d_model//n_heads)
         act = nn.ReLU()
+        if ff_dim==None: ff_dim=d_model*4
         self.ff = nn.Sequential(
             nn.RMSNorm(d_model), nn.Linear(d_model, ff_dim), act,
-            nn.RMSNorm(ff_dim), nn.Dropout(dropout), zero_module(nn.Linear(ff_dim, d_model))
+            nn.RMSNorm(ff_dim), zero_module(nn.Linear(ff_dim, d_model))
+            # nn.RMSNorm(d_model), act, nn.Linear(d_model, ff_dim),
+            # nn.RMSNorm(ff_dim), act, zero_module(nn.Linear(ff_dim, d_model))
         )
+        # self.ff = SwiGLU(d_model, ff_dim)
+        # torch.nn.init.normal_(self.ff[1].weight, std=.02)
+        torch.nn.init.normal_(self.ff[1].weight, std=1/(math.sqrt(d_model)+math.sqrt(ff_dim)))
 
-    def forward(self, x, mask=None):
-        x = x + self.drop(self.attn(self.norm(x), mask=mask))
-        x = x + self.drop(self.ff(x))
+    def forward(self, x, cond=None, mask=None): # [b,c,h,w], [batch, num_tok, cond_dim], [batch,T]
+        # print('attblk fwd', x.shape, cond.shape if cond!=None else None, mask.shape if mask!=None else None)
+        if self.cond_dim==None: x = x + self.attn(self.norm1(x), mask=mask)
+        else: x = x + self.attn(self.norm1(x), self.norm2(cond), mask) # maybe no res for decoder
+        x = x + self.ff(x) # maybe no ff for decoder?
         return x
+
+import inspect
+class Seq(nn.Sequential):
+    def __init__(self, *args):
+        super().__init__(*args)
+        for layer in self:
+            params = inspect.signature(layer.forward).parameters.keys()
+            layer._fwdparams = ','.join(params)
+
+    def forward(self, x, cond=None, mask=None):
+        for layer in self:
+            args = [x]
+            if 'cond' in layer._fwdparams: args.append(cond)
+            if 'mask' in layer._fwdparams: args.append(mask)
+            x = layer(*args)
+        return x
+
+# b,t,d = 2,5,16
+# x = torch.rand(b,t,d)
+# mask = torch.rand(b,t,t)>0
+# model = AttentionBlock(d_model=d, n_heads=4, ff_dim=16)
+# model = nn.Sequential(*[AttentionBlock(d_model=d, n_heads=4, ff_dim=16) for _ in range(2)])
+# out =  model(x, mask)
+# # out =  model(x)
+# print(out.shape)
