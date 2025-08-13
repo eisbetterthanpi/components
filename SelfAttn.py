7b68ef213819bc011d12bb245c8ec513424e60c6
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-08-13 17:48:40 +0800

    Update SelfAttn.py

--- SelfAttn.py
+++ SelfAttn.py
@@ -13,15 +13,17 @@ class SelfAttn(nn.Module):
         self.dim, self.heads = dim, n_heads
         d_head = dim//n_heads
         self.qkv = nn.Linear(dim, dim*3, bias=False)
+        self.rope = RoPE(d_head, seq_len=512, base=10000)
+        self.scale = d_head**-.5
         # self.lin = nn.Linear(dim, dim)
         self.lin = zero_module(nn.Linear(dim, dim))
-        self.rope = RoPE(d_head, seq_len=512, base=10000)
-        # self.rope = RoPE2D(d_head, h=64, w=64, base=100)
-        self.scale = d_head**-.5
 
     def forward(self, x): # [b,t,d]
-        q,k,v = self.qkv(x).unflatten(-1, (self.heads,-1)).chunk(3, dim=-1) # [b, r^2, h/r*w/r, dim] # [b*r^2, h/r*w/r, n_heads, d_head]?
+        q,k,v = self.qkv(x).chunk(3, dim=-1)
         q, k = self.rope(q), self.rope(k)
+        q = q.unflatten(-1, (self.heads,-1)).transpose(1,2) # [b,h,t,d]
+        k = k.unflatten(-1, (self.heads,-1)).transpose(1,2)
+        v = v.unflatten(-1, (self.heads,-1)).transpose(1,2)
         q, k = q.softmax(dim=-1)*self.scale, k.softmax(dim=-2)
         context = k.transpose(-2,-1) @ v # [batch, n_heads, d_head, d_head]
         x = q @ context # [batch, n_heads, T/num_tok, d_head]
@@ -42,11 +44,15 @@ class SelfAttn(nn.Module):
 
     def forward(self, x): # [b,c,h,w]
         b,c,h,w = x.shape
-        x = x.flatten(2).transpose(-2,-1)
+        x = x.flatten(2).transpose(-2,-1) # [b,t,d]
         # x = F.pixel_unshuffle(x.transpose(0,1), self.r).flatten(2).permute(1,2,0) # [b,c,h,w] -> [c,b*r^2,h/r,w/r] -> [b,h/r*w/r,c]
 
-        q,k,v = self.qkv(x).unflatten(-1, (self.heads,-1)).chunk(3, dim=-1) # [b, r^2, h/r*w/r, dim] # [b*r^2, h/r*w/r, n_heads, d_head]?
+        # q,k,v = self.qkv(x).unflatten(-1, (self.heads,-1)).chunk(3, dim=-1) # [b, r^2, h/r*w/r, dim] # [b*r^2, h/r*w/r, n_heads, d_head]?
+        q,k,v = self.qkv(x).chunk(3, dim=-1) # [b, r^2, h/r*w/r, dim] # [b*r^2, h/r*w/r, n_heads, d_head]?
         q, k = self.rope(q), self.rope(k)
+        q = q.unflatten(-1, (self.heads,-1)).transpose(1,2) # [b,h,t,d]
+        k = k.unflatten(-1, (self.heads,-1)).transpose(1,2)
+        v = v.unflatten(-1, (self.heads,-1)).transpose(1,2)
 
         q, k = q.softmax(dim=-1)*self.scale, k.softmax(dim=-2)
         context = k.transpose(-2,-1) @ v # [batch, n_heads, d_head, d_head]
